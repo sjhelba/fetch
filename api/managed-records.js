03241ef7d98731a0c76bf219808486afce230728
Sara Jo Helba <29603156+sjhelba@users.noreply.github.com>
2017-11-30 13:22:31 -0500

    completed

--- api/managed-records.js
+++ api/managed-records.js
@@ -5,5 +5,41 @@ import URI from "urijs";
 window.path = "http://localhost:3000/records";
 
 // Your retrieve function plus any additional functions go here ...
+const isPrimaryColor = color => ['red', 'blue', 'yellow'].indexOf(color) !== -1;
+const createReqUri = (idx, colors) => {
+  const uri = new URI(window.path)
+    .addQuery('limit', '11')
+    .addQuery('offset', idx);
+  if (colors) uri.addQuery('color[]', colors);
+  return uri;
+};
+
+const retrieve = (options = {}) => {
+  const {colors, page = 1} = options;
+  const index = (page - 1) * 10;
+  const result = {ids: [], open: [], closedPrimaryCount: 0};
+  const reqUri = createReqUri(index, colors);
+
+  return fetch(reqUri)
+    .then(res => res.json())
+    .then(data => {
+      const nextPageItem = data.splice(10, 1);
+      result.previousPage = page - 1 || null;
+      result.nextPage = data.length && nextPageItem.length ? page + 1 : null;
+
+      data.forEach(item => {
+        result.ids.push(item.id);
+        item.isPrimary = isPrimaryColor(item.color);
+        if (item.disposition === 'open'){
+          result.open.push(item);
+        } else if (item.isPrimary){
+          result.closedPrimaryCount++;
+        }
+      });
+
+      return result;
+    })
+    .catch(err => console.log('Fetch Request Failed', err));
+};
 
 export default retrieve;
